Need to fix preprocessing, gradients for discriminator are unstable because the image features are too big. Normalization required.

discriminator.py
@@ -16,10 +16,9 @@ class Discriminator(object):
         self.maxlen = n_lstm_steps
 
         xavier_initializer = tf.contrib.layers.xavier_initializer()
-        constant_initializer = tf.constant_initializer(0.0)
-        #Initialize the word embeddings
-        with tf.device("/cpu:0"):
-            self.Wemb = tf.get_variable("Wemb", [vocab_size, dim_embed], initializer=xavier_initializer)
+        constant_initializer = tf.constant_initializer(0.05)
+        """#Initialize the word embeddings
+        self.Wemb = tf.get_variable("Wemb", [vocab_size, dim_embed], initializer=xavier_initializer)
 
         #Initial hidden state of LSTM
         self.init_hidden_W = tf.get_variable("init_hidden_W", [dim_context, dim_hidden], initializer=xavier_initializer)
@@ -56,7 +55,19 @@ class Discriminator(object):
         #(i.e slap a softmax on it and the output is a probability for each word in the vocab being
         #correct at that timestep)
         self.decode_word_W = tf.get_variable("decode_word_W", [dim_embed, 1], initializer=xavier_initializer)
-        self.decode_word_b = tf.get_variable("decode_word_b", [1], initializer=constant_initializer)
+        self.decode_word_b = tf.get_variable("decode_word_b", [1], initializer=constant_initializer)"""
+        self.Wemb = tf.get_variable("Wemb", [vocab_size, dim_embed], initializer=xavier_initializer)
+        self.embed_triple_W = tf.get_variable("embed_triple_W", [self.maxlen*dim_embed, dim_embed], initializer=xavier_initializer)
+        self.embed_triple_b = tf.get_variable("embed_triple_b", [dim_embed], initializer=constant_initializer)
+
+        self.embed_image_W = tf.get_variable("embed_image_W", [context_shape[0]*context_shape[1], dim_embed], initializer=xavier_initializer)
+        self.embed_image_b = tf.get_variable("embed_image_b", [dim_embed], initializer=constant_initializer)
+
+        self.out_W_1 = tf.get_variable("out_W_1", [dim_embed*2, dim_embed], initializer=xavier_initializer)
+        self.out_b_1 = tf.get_variable("out_b_1", [dim_embed], initializer=constant_initializer)
+
+        self.out_W_2 = tf.get_variable("out_W_2", [dim_embed, 1], initializer=xavier_initializer)
+        self.out_b_2 = tf.get_variable("out_b_2", [1], initializer=constant_initializer)
 
 
     def get_initial_lstm(self, mean_context):
@@ -68,7 +79,7 @@ class Discriminator(object):
 
         return initial_hidden, initial_memory
 
-    def build_discriminator(self, context, triples):
+    """def build_discriminator(self, context, triples):
         #The context vector (\hat{z_t} in the paper) is a dynamic representation of a relevant part of an image 
         #at time t
         #Here however, the context has more dimensions. The batch_size is obvious, but the context shape refers
@@ -156,8 +167,51 @@ class Discriminator(object):
 
         #word_probs = tf.stack(l)
         #word_probs = tf.transpose(word_probs, [1, 0, 2])
-        return logit_words
+        return logit_words"""
 
+    
+    """def build_discriminator(self, context, triples):
+        #Embed the triple
+        flattened_seq = tf.reshape(triples, [self.batch_size*self.maxlen, self.vocab_size])
+        embedded_seq = tf.matmul(flattened_seq, self.Wemb)
+        embedded_seq = tf.reshape(embedded_seq, [self.batch_size, self.maxlen, self.dim_embed])
+        embedded_seq = tf.reshape(embedded_seq, [self.batch_size, self.maxlen*self.dim_embed])
+        embedded_seq = tf.add(tf.matmul(embedded_seq, self.embed_triple_W), self.embed_triple_b)
+
+        #Embed the image features
+        flattened_im = tf.reshape(context, [self.batch_size, self.context_shape[0]*self.context_shape[1]])
+        embedded_im = tf.add(tf.matmul(flattened_im, self.embed_image_W), self.embed_image_b)
+
+        #Concatenate
+        concatenated = tf.concat([embedded_seq, embedded_im], 1)
+
+        #Run concatenated vector through output MLP
+        h1 = tf.add(tf.matmul(concatenated, self.out_W_1), self.out_b_1)
+        h2 = tf.add(tf.matmul(h1, self.out_W_2), self.out_b_2)
+        return h2"""
+
+    def ResBlock(self, name, inputs):
+        output = inputs
+        output = tf.nn.relu(output)
+        
+        output = lib.ops.conv1d.Conv1D(name+'.1', self.DIM, self.DIM, 5, output)
+        output = tf.nn.relu(output)
+        output = lib.ops.conv1d.Conv1D(name+'.2', self.DIM, self.DIM, 5, output)
+        return inputs + (0.3*output)
+
+
+    def build_discriminator(self, context, triples):
+        output = inputs
+        output = tf.nn.relu(output)
+        #output = lib.ops.conv1d.Conv1D(name+'.1', self.DIM, self.DIM, 5, output)
+        output = tf.nn.conv1d(output, self.conv_1, stride=1, padding="VALID")
+        output = tf.nn.relu(output)
+        #output = lib.ops.conv1d.Conv1D(name+'.2', self.DIM, self.DIM, 5, output)
+        output = tf.nn.conv1d(output, self.conv_2, stride=1, padding="VALID")
+        return inputs + (0.3*output)
+
+
+        
 
 
 if __name__ == "__main__":

generator.py
@@ -23,12 +23,12 @@ class Generator(object):
         self.n_lstm_steps = n_lstm_steps
         self.batch_size = batch_size
         self.maxlen = n_lstm_steps
+        self.noise_dim = 128
 
         xavier_initializer = tf.contrib.layers.xavier_initializer()
-        constant_initializer = tf.constant_initializer(0.0)
-        #Initialize the word embeddings
-        with tf.device("/cpu:0"):
-            self.Wemb = tf.get_variable("Wemb", [vocab_size, dim_embed], initializer=xavier_initializer)
+        constant_initializer = tf.constant_initializer(0.05)
+        """#Initialize the word embeddings
+        self.Wemb = tf.get_variable("Wemb", [vocab_size, dim_embed], initializer=xavier_initializer)
 
         #Initial hidden state of LSTM
         self.init_hidden_W = tf.get_variable("init_hidden_W", [dim_context, dim_hidden], initializer=xavier_initializer)
@@ -65,7 +65,29 @@ class Generator(object):
         #(i.e slap a softmax on it and the output is a probability for each word in the vocab being
         #correct at that timestep)
         self.decode_word_W = tf.get_variable("decode_word_W", [dim_embed, vocab_size], initializer=xavier_initializer)
-        self.decode_word_b = tf.get_variable("decode_word_b", [vocab_size], initializer=constant_initializer)
+        self.decode_word_b = tf.get_variable("decode_word_b", [vocab_size], initializer=constant_initializer)"""
+
+        #The weights for encoding the context to feed it into the attention MLP
+        #Needs to be encoded in order to combine it with the previous hidden state
+        self.context_encode_W = tf.get_variable("context_encode", [self.context_shape[0]*self.context_shape[1], self.dim_context*2], initializer=xavier_initializer)
+
+        self.Wemb = tf.get_variable("Wemb", [self.vocab_size, self.dim_embed], initializer=xavier_initializer)
+
+        self.att_W = tf.get_variable("att_W", [(self.dim_context*2)+self.dim_hidden+self.noise_dim, self.context_shape[0]], initializer=xavier_initializer)
+        self.att_b = tf.get_variable("att_b", [self.context_shape[0]], initializer=constant_initializer)
+
+        self.lstm_W = tf.get_variable("lstm_W", [self.dim_context+self.dim_hidden+self.dim_embed, self.dim_hidden*4], initializer=xavier_initializer)
+        self.lstm_b = tf.get_variable("lstm_b", [self.dim_hidden*4], initializer=constant_initializer)
+
+        self.init_hidden_W = tf.get_variable("init_hidden_W", [self.dim_context, self.dim_hidden], initializer=xavier_initializer)
+        self.init_hidden_b = tf.get_variable("init_hidden_b", [self.dim_hidden], initializer=constant_initializer)
+
+        self.init_memory_W = tf.get_variable("init_memory_W", [self.dim_context, self.dim_hidden], initializer=xavier_initializer)
+        self.init_memory_b = tf.get_variable("init_memory_b", [self.dim_hidden], initializer=constant_initializer)
+
+        self.decode_lstm_W = tf.get_variable("decode_lstm_W", [self.dim_hidden, self.vocab_size], initializer=xavier_initializer)
+        self.decode_lstm_b = tf.get_variable("decode_lstm_b", [self.vocab_size], initializer=constant_initializer)
+
 
 
     def get_initial_lstm(self, mean_context):
@@ -77,7 +99,7 @@ class Generator(object):
 
         return initial_hidden, initial_memory
 
-    def build_generator(self, context):
+    """def build_generator(self, context):
         #The context vector (\hat{z_t} in the paper) is a dynamic representation of a relevant part of an image 
         #at time t
         #Here however, the context has more dimensions. The batch_size is obvious, but the context shape refers
@@ -112,10 +134,9 @@ class Generator(object):
                 word_emb = tf.zeros([self.batch_size, self.dim_embed])
             else:
                 tf.get_variable_scope().reuse_variables()
-                with tf.device("/cpu:0"):
-                    #word_emb = tf.nn.embedding_lookup(self.Wemb, sentence[:,ind-1])
-                    #The calculation will be made based on the last word looked at
-                    word_emb = tf.nn.embedding_lookup(self.Wemb, word_prediction)
+                #word_emb = tf.nn.embedding_lookup(self.Wemb, sentence[:,ind-1])
+                #The calculation will be made based on the last word looked at
+                word_emb = tf.nn.embedding_lookup(self.Wemb, word_prediction)
 
             #This is the T_{D+m+n,n} affine transformation referred to in section 3.1.2
             x_t = tf.matmul(word_emb, self.lstm_W) + self.lstm_b # (batch_size, hidden*4)
@@ -170,7 +191,65 @@ class Generator(object):
 
         word_probs = tf.stack(l)
         word_probs = tf.transpose(word_probs, [1, 0, 2])
+        return word_probs"""
+
+    def build_generator(self, context):
+        #From the paper: The initial memory state and hidden state of the LSTM are predicted
+        #by an average of the annotation vectors fed through two separate MLPs
+        h, c = self.get_initial_lstm(tf.reduce_mean(context, 1))#(batch_size, dim_hidden)
+
+        
+        noise = tf.random_normal([self.batch_size, self.noise_dim])
+
+        l = []
+
+        for ind in range(self.n_lstm_steps):
+            if ind == 0:
+                word_emb = tf.zeros([self.batch_size, self.dim_embed])
+            else:
+                word_emb = tf.nn.embedding_lookup(self.Wemb, word_prediction)
+            #Calculate \hat{z}
+            #Equation (4)
+            #Concatenate flattened context with previous hidden state and the noise vector, and feed through attention mlp
+            flattened_context = tf.reshape(context, [self.batch_size, self.context_shape[0]*self.context_shape[1]])
+            encoded_context = tf.matmul(flattened_context, self.context_encode_W)
+            context_and_hidden_state = tf.concat([encoded_context, h, noise], 1)
+            e = tf.add(tf.matmul(context_and_hidden_state, self.att_W), self.att_b)
+            #Equation (5)
+            alpha = tf.nn.softmax(e)
+            #Equation (6) and (13): Soft attention model
+            z_hat = tf.reduce_sum(context * tf.expand_dims(alpha, 2), 1) #Output is [batch size, D]
+            #Equation (1)
+            #Concatenate \hat{z}_t , h_{t-1}, and the embedding of the previous word 
+            lstm_input = tf.concat([z_hat, h, word_emb], 1)
+            #Perform affine transformation of concatenated vector
+            affine = tf.add(tf.matmul(lstm_input, self.lstm_W), self.lstm_b)
+            i, f, o, g = tf.split(affine, 4, 1)
+            #i_t = sigmoid(affine)
+            i = tf.nn.sigmoid(i)
+            #f_t = sigmoid(affine)
+            f = tf.nn.sigmoid(f)
+            #o_t = sigmoid(affine)
+            o = tf.nn.sigmoid(o)
+            #g_t = tanh(affine)
+            g = tf.nn.tanh(g)
+
+            #Equation (2)
+            #c_t = f_t * c_{t-1} + i_t * g_t
+            c = f * c + i * g
+            #h_t = o_t * tanh(c_t)
+            h = o * tf.nn.tanh(c)
+
+            logits = tf.add(tf.matmul(h, self.decode_lstm_W), self.decode_lstm_b)
+            word_prob = tf.nn.softmax(logits)
+            word_prediction = tf.argmax(logits, 1)
+            l.append(word_prob)
+
+        word_probs = tf.stack(l)
+        #We want it in shape [batch_size, seq_len, vocab_size]
+        word_probs = tf.transpose(word_probs, [1,0,2])
         return word_probs
+
 
 if __name__ == "__main__":
     g = Generator(10000)

my_gan.py
@@ -16,6 +16,7 @@ import tflib.plot
 from tqdm import tqdm
 from generator import Generator
 from discriminator import Discriminator
+from subprocess import call
 
 
 class SceneGraphWGAN(object):
@@ -24,6 +25,18 @@ class SceneGraphWGAN(object):
         self.batch_path += "/" if self.batch_path[-1] != "/" else ""
         self.path_to_vocab_json = path_to_vocab_json
         self.path_to_vocab_json += "/" if self.path_to_vocab_json != "/" else ""
+        self.logs_dir = "./logs/"
+        self.checkpoints_dir = os.path.join(self.logs_dir, "checkpoints/")
+        self.summaries_dir = os.path.join(self.logs_dir, "summaries/")
+
+        if not os.path.exists(self.checkpoints_dir):
+            os.makedirs(self.checkpoints_dir)
+
+        if not os.path.exists(self.summaries_dir):
+            os.makedirs(self.summaries_dir)
+
+        
+        call(["rm", "{}*".format(self.summaries_dir)])
 
         #Calculating vocabulary and sequence lengths
         with open(path_to_vocab_json, "r") as f:
@@ -38,7 +51,7 @@ class SceneGraphWGAN(object):
         #Hyperparameters
         self.BATCH_SIZE = BATCH_SIZE
         self.LAMBDA = 10
-        self.CRITIC_ITERS = 10
+        self.CRITIC_ITERS = 25
         self.DIM = 512
         self.ITERS = 100000
 
@@ -118,6 +131,9 @@ class SceneGraphWGAN(object):
         disc_cost = tf.reduce_mean(disc_fake) - tf.reduce_mean(disc_real)
         gen_cost = -tf.reduce_mean(disc_fake)
 
+        tf.summary.scalar("Discriminator Cost", disc_cost)
+        tf.summary.scalar("Generator Cost", gen_cost)
+
         # WGAN lipschitz-penalty
         alpha = tf.random_uniform(
             shape=[self.BATCH_SIZE,1,1], 
@@ -138,8 +154,24 @@ class SceneGraphWGAN(object):
         gen_params = [v for v in train_variables if v.name.startswith("Generator")]
         disc_params = [v for v in train_variables if v.name.startswith("Discriminator")]
 
-        self.gen_train_op = tf.train.AdamOptimizer(learning_rate=1e-4, beta1=0.5, beta2=0.9).minimize(gen_cost, var_list=gen_params)
-        self.disc_train_op = tf.train.AdamOptimizer(learning_rate=1e-4, beta1=0.5, beta2=0.9).minimize(disc_cost, var_list=disc_params)
+        optimizer = tf.train.AdamOptimizer(learning_rate=1e-4, beta1=0.5, beta2=0.9)
+
+        gen_grads = optimizer.compute_gradients(gen_cost, var_list=gen_params)
+        disc_grads = optimizer.compute_gradients(disc_cost, var_list=disc_params)
+
+        #self.gen_train_op = tf.train.AdamOptimizer(learning_rate=1e-4, beta1=0.5, beta2=0.9).minimize(gen_cost, var_list=gen_params)
+        #self.disc_train_op = tf.train.AdamOptimizer(learning_rate=1e-4, beta1=0.5, beta2=0.9).minimize(disc_cost, var_list=disc_params)
+
+        self.gen_train_op = optimizer.apply_gradients(gen_grads)
+        self.disc_train_op = optimizer.apply_gradients(disc_grads)
+
+        for grad, var in gen_grads:
+            if grad is not None:
+                tf.summary.histogram(var.op.name + "/gradient", grad)
+
+        for grad, var in disc_grads:
+            if grad is not None:
+                tf.summary.histogram(var.op.name + "/gradient", grad)
 
     def DataGenerator(self):
         filenames = ["{}{}".format(self.batch_path, i) for i in os.listdir(self.batch_path)]
@@ -164,11 +196,27 @@ class SceneGraphWGAN(object):
                 del indices[-self.BATCH_SIZE:]
                 yield im_batch, t_batch
 
+    """def generateSamples(self, session):
+        #Load features from a specific image
+        ##Load the image
+        path_to_image = "/home/mklawonn/visual_genome/all_images/0.jpg"
+        ##Extract features
+        ##Load the image in a tensor
+        #Generate triples from this image
+        
+        #Write a summary that puts the image and ground truth in tensorboard
+        #Write a summary that puts generated triples into tensorboard"""
+
 
     def Train(self, epochs):
+        self.saver = tf.train.Saver()
         self.Loss()
+        summary_op = tf.summary.merge_all()
         start_time = time.time()
         with tf.Session() as session:
+            #self.generateSamples(session)
+            writer = tf.summary.FileWriter(self.summaries_dir, session.graph)
+
 
             session.run(tf.global_variables_initializer())
 
@@ -185,40 +233,46 @@ class SceneGraphWGAN(object):
 
             gen = self.DataGenerator()
 
-            iteration = 0
-            for im_batch, triple_batch in self.DataGenerator():
-                #Train Generator
-                if iteration > 0:
-                    _ = session.run(self.gen_train_op, feed_dict={self.image_feats:im_batch})
+            for epoch in range(epochs):
+                iteration = 0
+                for im_batch, triple_batch in self.DataGenerator():
+                    #Train Generator
+                    if iteration > 0:
+                        _ = session.run(self.gen_train_op, feed_dict={self.image_feats:im_batch})
 
-                #Train Critic
-                for i in xrange(self.CRITIC_ITERS):
-                    #im_batch, triple_batch = gen.next()
-                    _disc_cost, _ = session.run(
-                        [self.disc_cost, self.disc_train_op],
-                        feed_dict={self.real_inputs:triple_batch, self.image_feats:im_batch}
-                    )
+                    #Train Critic
+                    for i in xrange(self.CRITIC_ITERS):
+                        #im_batch, triple_batch = gen.next()
+                        _disc_cost, _ = session.run(
+                            [self.disc_cost, self.disc_train_op],
+                            feed_dict={self.real_inputs:triple_batch, self.image_feats:im_batch}
+                        )
 
-                if iteration % 200 == 0:
+                    if iteration % 5 == 0:
 
-                    stop_time = time.time()
-                    duration = (stop_time - start_time) / 200.0
-                    start_time = stop_time
-                    _gen_cost = session.run(self.gen_cost, feed_dict={self.image_feats:im_batch})
+                        """stop_time = time.time()
+                        duration = (stop_time - start_time) / 200.0
+                        start_time = stop_time"""
+                        summary, _gen_cost = session.run([summary_op, self.gen_cost], feed_dict={self.real_inputs:triple_batch, self.image_feats:im_batch})
+                        writer.add_summary(summary, iteration)
+                        writer.flush()
 
-                    print "Time {}/itr, Step: {}, generator loss: {}, discriminator loss: {}".format(
-                            duration, iteration, _gen_cost, _disc_cost)
+                        """print "Time {}/itr, Step: {}, generator loss: {}, discriminator loss: {}".format(
+                                duration, iteration, _gen_cost, _disc_cost)"""
 
-                if iteration % 5000 == 0:
-                    samples = []
-                    for i in xrange(10):
-                        samples.extend(generate_samples(im_batch))
+                    if iteration % 25 == 0:
+                        samples = []
+                        for i in xrange(10):
+                            samples.extend(generate_samples(im_batch))
 
-                    with open('./samples/samples_{}.txt'.format(iteration), 'w') as f:
-                        for s in samples:
-                            s = " ".join(s)
-                            f.write(s + "\n")
-                iteration += 1
+                        with open('./samples/samples_{}.txt'.format(iteration), 'w') as f:
+                            for s in samples:
+                                s = " ".join(s)
+                                f.write(s + "\n")
+
+                        
+                        self.saver.save(session, os.path.join(self.checkpoints_dir, "model.ckpt"), global_step=(epoch+1)*iteration)
+                    iteration += 1
 
 
 if __name__ == "__main__":